9cc627ab24d7e3641d3634c7664349c9ea597d2f
firewaterge <gyczt879574764@gmail.com>
2017-02-26 08:34:03 -0500

    add express framework and node server

--- public/server.js
+++ public/server.js
@@ -1,44 +1,22 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
 
-// Create a server
-http.createServer( function (request, response) {
-   // Parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
 
-   // Print the name of the file for which request is made.
-//   console.log("Request for " + pathname + " received.");
+app.get('/css/:file',function(req,res){
+    res.sendFile(__dirname + '/css/' + req.params['file']);
+});
 
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else {
-         //Page found
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         switch(pathname){
-           case '/public/html.index':
-             response.writeHead(200, {'Content-Type': 'text/html'});
-             break;
-           case '/public/css/style.css':
-             response.writeHead(200, {'Content-Type': 'text/css'});
-             break;
-           case '/public/javascript/*':
-             response.writeHead(200, {'Content-Type': 'text/javascript'});
-             break;
-         }
-         // Write the content of the file to response body
-         response.write(data.toString());
-      }
-      // Send the response body
-      response.end();
-   });
-}).listen(80);
+app.get('/javascripts/:file',function(req,res){
+    res.sendFile(__dirname + '/javascripts/' + req.params['file']);
+});
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:80/');
+app.get('/articles/:file',function(req,res){
+    res.sendFile(__dirname + '/articles/' + req.params['file']);
+});
+
+var server = app.listen(80,function(){
+    console.log('server running');
+});
