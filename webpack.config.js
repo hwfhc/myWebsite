ac09c5ededde9706ec3770cd13e3e1b7f237e527
firewaterge <gyczt879574764@gmail.com>
2017-02-26 08:34:45 -0500

    debug

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ module.exports = {
     entry: __dirname + "/app/index.js",
     output: {
         path: __dirname + "/public",
-        filename: "bundle.js"
+        filename: "javascripts/bundle.js"
     }
 }
