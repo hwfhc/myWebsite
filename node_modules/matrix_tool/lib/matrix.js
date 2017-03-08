ea3f7ebb5db1386cd31f01828ab25fbaa8faf562
firewaterge <gyczt879574764@gmail.com>
2017-03-08 09:51:26 -0500

    update matrix_tool module

--- node_modules/matrix_tool/lib/matrix.js
+++ node_modules/matrix_tool/lib/matrix.js
@@ -5,17 +5,17 @@ function Matrix(VALUE){
      *效果说明:
      *创建一个矩阵
      */
-     if(VALUE.length != 0){
-       this.value = VALUE;
-       this.row = VALUE.length;
-       this.column = VALUE[0].length;
-     }
-     else{
-       console.log("矩阵不能无元素");
-     }
-  }
+    if(VALUE.length != 0){
+        this.value = VALUE;
+        this.row = VALUE.length;
+        this.column = VALUE[0].length;
+    }
+    else{
+        console.error("[matrix_tool] there is no element in matrix");
+    }
+}
 
-  Matrix.prototype.Add = function(M){
+Matrix.prototype.Add = function(M){
     /*
      *效果说明:
      *矩阵相加，在本矩阵基础上加上M矩阵,获得一个新的矩阵
@@ -30,26 +30,26 @@ function Matrix(VALUE){
 
     if(this.row == M.row && this.column == M.column)//同型矩阵判断
     {
-      for(row = 0;row < this.row; row++)
-      {
-        //初始化每一行的数组，否则无法使用数组脚标(js没有二维数组)
-        OutPut.value[row] = [];
-
-        for(column = 0;column < this.column; column++)
+        for(row = 0;row < this.row; row++)
         {
-          OutPut.value[row][column] = this.value[row][column] + M.value[row][column];
+            //初始化每一行的数组，否则无法使用数组脚标(js没有二维数组)
+            OutPut.value[row] = [];
+
+            for(column = 0;column < this.column; column++)
+            {
+                OutPut.value[row][column] = this.value[row][column] + M.value[row][column];
+            }
         }
-      }
     }
     else
     {
-      console.log('矩阵相加异常');
+        console.error('[matrix_tool] columns and row of two matrix must be same');
     }
 
     return OutPut;
-  }
+}
 
-  Matrix.prototype.Multiple = function(M){
+Matrix.prototype.Multiple = function(M){
     /*
      *效果说明:
      *矩阵相乘，在本矩阵基础上右乘M矩阵，获得新的矩阵
@@ -69,51 +69,48 @@ function Matrix(VALUE){
 
     if(this.column == M.row)//列数等于行数
     {
-      for(row = 0;row < this.row;row++)
-      {
-        //初始化每一行的数组，否则无法使用数组脚标(js没有二维数组)
-        OutPut.value[row] = [];
-
-        for(column = 0;column < this.column; column++)
+        for(row = 0;row < this.row;row++)
         {
-          sum = 0;
+            //初始化每一行的数组，否则无法使用数组脚标(js没有二维数组)
+            OutPut.value[row] = [];
 
-          for(i = 0;i < this.column;i++)
-          {
-            sum += this.value[row][i] * M.value[i][column];
-          }
+            for(column = 0;column < this.column; column++)
+            {
+                sum = 0;
 
-          OutPut.value[row][column] = sum;
+                for(i = 0;i < this.column;i++)
+                {
+                    sum += this.value[row][i] * M.value[i][column];
+                }
+
+                OutPut.value[row][column] = sum;
+            }
         }
-      }
     }
     else
     {
-      console.log('矩阵相乘异常');
+        console.error('[matrix_tool] columns and rows must be same');
     }
     //设置矩阵行，列
     OutPut.ReFreshRC();
     return OutPut;
-  }
+}
 
-  Matrix.prototype.GetNumber = function(row,column){
+Matrix.prototype.GetNumber = function(row,column){
     if(row <= this.row && column <= this.column && row > 0 && column >0){
-      return this.value[row-1][column-1];
+        return this.value[row-1][column-1];
     }
     else{
-      console.log("获取矩阵数值失败");
+        console.error("[matrix_tool] can not get the number");
     }
-  }
+}
 
-  Matrix.prototype.ReFreshRC = function(){
-   /*
-    *效果说明：
-    *重新计算矩阵的宽和列
-    */
+Matrix.prototype.ReFreshRC = function(){
+    /*
+     *效果说明：
+     *重新计算矩阵的宽和列
+     */
     this.row = this.value.length;
     this.column = this.value[0].length;
-  }
-
-function Location(x,y){
-
+}
 }
